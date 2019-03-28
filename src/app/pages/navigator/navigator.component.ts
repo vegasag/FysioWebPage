17167ea1f4d8d7d43cbd3bcec0142388eae19cb0
Vegard Saga <vegard>
2019-03-28 15:29:51 +0100

    Makes header responsive to scroll. When y-pos is below 100px, header gains box shadow

--- src/app/pages/navigator/navigator.component.ts
+++ src/app/pages/navigator/navigator.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
-  styleUrls: ['./navigator.component.css']
+  styleUrls: ['./navigator.component.scss'],
 })
 export class NavigatorComponent implements OnInit {
   navLinks = [
@@ -12,7 +12,7 @@ export class NavigatorComponent implements OnInit {
     { displayName: 'Kontakt oss', href: '#ContactUs' },
   ];
   isOpen: boolean = false; 
-
+  shadowLineVisible: boolean = false;
   constructor() { }
 
   ngOnInit() {}
@@ -21,6 +21,18 @@ export class NavigatorComponent implements OnInit {
   }
   toggleOpen(){
     this.isOpen = !this.isOpen;
+    console.log(window.pageYOffset);
   }
 
+  @HostListener("window:scroll",[])
+  
+  onWindowScroll(){
+    let number = window.pageYOffset || document.documentElement.scrollTop
+    if (number < 100) {
+      this.shadowLineVisible = false;
+    }
+    else if (number > 10) {
+      this.shadowLineVisible = true;
+    }
+  }
 }
